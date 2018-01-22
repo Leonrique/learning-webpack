92b9ccec72ca4853f879cb5aa003af03d6e4e6b3
Leonrique <leonrick777@gmail.com>
2018-01-22 17:40:24 -0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header/>
+
+    <img src="./assets/logo.png">
+
+    <HelloWorld/>
+    <br>
+
+    <Greeting/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Greeting from './components/Greeting'
+import Header from './components/Header'
+/* import 'bootstrap' */
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Greeting, Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body{
+  /* background-color: rgb(167, 156, 156) */
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1 id="greeting">Just working fine - Header component single file</h1>
+      <input type="text" class="form-control">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#greeting {
+  color: bisque
+}
+</style>
