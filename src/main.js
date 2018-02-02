d8fb18d3271f6f0bc1ec13380b76ed8daa174e47
Leonrique <leonrick777@gmail.com>
2018-02-02 14:40:48 -0200

    demo

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import Vue from "vue";
 import VueRouter from "vue-router";
-import VueFire from "vuefire"
+//import VueFire from "vuefire"
+var VueFire = require('vuefire')
 import Main from "./Main.vue"
 import Home from "./Home.vue";
 import OtherStuff from "./OtherStuff.vue";
